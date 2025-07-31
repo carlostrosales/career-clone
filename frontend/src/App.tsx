0beda1eb0ab75999e70ad8b5dbd0c834dc612b20
carlostrosales <rosales.carlost@Gmail.com>
2025-07-30 22:35:57 -0500

    feat: define data types and primary ui component interfaces

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,59 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+/* defining data types */
+type User = {
+  firstName: string;
+  lastName: string;
+  avatarUrl?: string;
+}
+
+type Message = {
+  id: string;
+  content: string;
+  role: User;
+  userInfo?: string;
+}
+
+type Conversations = {
+  conversation: Array<Message>;
+}
+
+/* defining ui component interfaces */
+
+interface ChatUIProps {
+  isQuerying: boolean;
+  onSubmit: string;
+  placeholder: string;
+  disabled: boolean;
+  conversations: Conversations;
+  customSubmitIcon?: React.ReactElement;
+}
+
+interface ChatInputProps {
+  disabled: boolean;
+  onSubmit: string;
+  placeholder: string;
+  customSubmitIcon?: React.ReactElement;
+}
+
+interface ChatConversationProps {
+  conversations: Conversations;
+  isQuerying: boolean;
+}
+
+interface ChatMessageProps {
+  message: Message;
+}
+
+
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
